3b79aa5e6966923c39b6115575b0f9ab92879212
ChanMin Kim <halfmoondeer@kookmin.ac.kr>
2021-08-22 22:54:20 +0900

    Solve Roman To Integer.

---
+++ RomanToInteger.py
@@ -0,0 +1,18 @@
+'''
+LeetCode 13. Roman To Integer
+Language : Python
+'''
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+        answer = 0
+
+        for i in range(len(s) - 1):
+            if dic[s[i]] < dic[s[i + 1]]:
+                answer -= dic[s[i]]
+            else:
+                answer += dic[s[i]]
+        answer += dic[s[-1]]
+
+        return answer 

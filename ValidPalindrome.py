4c8bfc34317fdf24512665f5181dab879f5cf6f4
ChanMin Kim <halfmoondeer@kookmin.ac.kr>
2021-09-08 21:26:35 +0900

    Solve Valid Palindrome.

---
+++ ValidPalindrome.py
@@ -0,0 +1,18 @@
+'''
+LeetCode 125. Valid Palindrome
+Language : Python
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        tmp = []
+
+        for char in s:
+            if char.isalnum():
+                tmp.append(char.lower())
+
+        while len(tmp) > 1:
+            if tmp.pop(0) != tmp.pop():
+                return False
+
+        return True

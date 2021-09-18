a0b7e9f3d9337669f1f079adbaf66e08cd554d75
ChanMin Kim <halfmoondeer@kookmin.ac.kr>
2021-09-18 21:04:20 +0900

    Solve Reverse Bits.

---
+++ ReverseBits.py
@@ -0,0 +1,13 @@
+'''
+LeetCode 190. Reverse Bits
+Language : Python
+'''
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        num = bin(n)[2:]
+
+        while len(num) < 32:
+            num = '0' + num
+
+        return int(num[::-1], 2)

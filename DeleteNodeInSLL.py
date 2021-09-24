5fd7d1030e89263f590753dc47b609ee13c043b7
ChanMin Kim <halfmoondeer@kookmin.ac.kr>
2021-09-24 21:53:50 +0900

    Solve Delete Node in a Linked List.

---
+++ DeleteNodeInSLL.py
@@ -0,0 +1,12 @@
+'''
+LeetCode 237. Delete Node in a Linked List
+Language : Python
+'''
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val, node.next = node.next.val, node.next.next
